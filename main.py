5fe441daaf87f79b756d58a998e22467323b2f04
lean <leandrobloogadse@gmail.com>
2023-04-15 10:24:44 -0300

    V 1.2 BETA test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from time import time
+import time
 
 from command import connection_status, start_ffmpeg, ffmpeg_status, check_process, kill_process
 
@@ -12,13 +12,20 @@ GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # Function to be executed when the button is pressed
 def button_callback(channel):
 
-    # if there is internet connection, start ffmpeg
-    if connection_status() and not check_process('ffmpeg'):
-        ffmpeg = start_ffmpeg()
+    # Check if ffmpeg process is currently running
+    process = check_process('ffmpeg')
+    # Check if there is an active network connection
+    connection = connection_status()
+
+    # If ffmpeg process is currently running, kill it and wait for 10 seconds
+    if process:
+        kill_process('ffmpeg')
         time.sleep(10)
 
-    else:
-        kill_process('ffmpeg')
+    # If ffmpeg process is not running and there is an active network connection, start the ffmpeg process and wait for 10 seconds
+    if not process and connection:
+        ffmpeg = start_ffmpeg()
+        time.sleep(10)
 
 # Register callback function for event
 GPIO.add_event_detect(3, GPIO.FALLING, callback=button_callback, bouncetime=300)
